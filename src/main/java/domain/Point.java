b3ef094836de21508c6fd99954bc0ff2e8dc3ddd
0chil <0@chll.it>
2023-03-01 18:41:49 +0900

    feat(Point): 두 연결점을 합칠 수 있다

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -21,12 +21,25 @@ public class Point {
         return Bridge.EMPTY;
     }
 
-    public Point createNextWith(Bridge nextBridge) throws SerialBridgeException {
-        validateNotSerial(nextBridge);
+    public Point join(Point next) {
+        if (!this.direction.isStay() && !next.direction.isStay()) {
+            throw new IllegalStateException("양방향 연결점은 생성될 수 없습니다");
+        }
+        if (this.direction.isRight() || next.direction.isRight()) {
+            return new Point(Direction.RIGHT);
+        }
+        if (this.direction.isLeft() || next.direction.isLeft()) {
+            return new Point(Direction.LEFT);
+        }
+        return new Point(Direction.STAY);
+    }
+
+    public Point createNextWith(Bridge bridgeAfterNewPoint) throws SerialBridgeException {
+        validateNotSerial(bridgeAfterNewPoint);
         if (this.direction.isRight()) {
             return new Point(Direction.LEFT);
         }
-        if (nextBridge.doesExist()) {
+        if (bridgeAfterNewPoint.doesExist()) {
             return new Point(Direction.RIGHT);
         }
         return new Point(Direction.STAY);
